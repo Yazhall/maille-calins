a123d48dc92d50b83689d8685ed255eb040d7c0f
Mael V <mael.vigier@hesias.fr>
2026-07-19 13:21:35 +0200

    feat: fixtures Category, Product, User avec vidage manuel des collections Mongo

---
+++ backend/src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\DataFixtures;
+use App\Document\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\MongoDBException;
+use Doctrine\Persistence\ObjectManager;
+class CategoryFixtures extends Fixture
+{
+    public function __construct(private DocumentManager $documentManager)
+    {
+
+    }
+
+    /**
+     * @throws MongoDBException
+     * @throws \Throwable
+     */
+    public function load(ObjectManager $manager): void{
+        $this->documentManager->getRepository(Category::class)->createQueryBuilder()
+        ->remove()
+        ->getQuery()
+        ->execute();
+        ;
+        $categories = [
+            ['name' => 'Animaux de la forêt', 'slug' => 'animaux-foret', 'order' => 1],
+            ['name' => 'Décorations', 'slug' => 'decorations', 'order' => 2],
+            ['name' => 'Accessoires', 'slug' => 'accessoires', 'order' => 3],
+            ['name' => 'Nouveautés', 'slug' => 'nouveautes', 'order' => 4],
+        ];
+        foreach ($categories as $data) {
+            $category = new Category();
+            $category->setName($data['name']);
+            $category->setSlug($data['slug']);
+            $category->setOrder($data['order']);
+            $category->setDescription('Description de la catégorie'.$data['name']);
+            $this->documentManager->persist($category);
+
+
+        }
+        $this->documentManager->flush();
+
+    }
+
+}
